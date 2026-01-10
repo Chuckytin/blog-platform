ba717e9d38f91d3f49eff9a834c0fdad17f79eeb
Chuckytin <angeltorcalcatalan@gmail.com>
2026-01-10 18:19:10 +0100

    v0.5.1 Error corregido de libreria

--- backend/src/main/java/com/example/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/example/backend/repositories/UserRepository.java
@@ -4,12 +4,10 @@ import com.example.backend.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.ScopedValue;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-
     Optional<User> findByEmail(String email);
 }
